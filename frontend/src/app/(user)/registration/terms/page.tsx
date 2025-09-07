66596123521be6dc7719669909870890a39277b5
ChayaphonKultanon <chayaphon33793@gmail.com>
2025-09-07 16:10:04 +0700

    finished terms page

--- frontend/src/app/(user)/registration/terms/page.tsx
+++ frontend/src/app/(user)/registration/terms/page.tsx
@@ -1,9 +1,11 @@
-import RegistrationName from "@/components/registration/registration_name";
+import RegistrationTerms from "@/components/registration/registration_terms";
+
+  
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center h-screen">
-        <RegistrationName />
+        <RegistrationTerms />
     </div>
   );
 }
