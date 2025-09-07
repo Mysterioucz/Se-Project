549162a1b231e5e484539de935d36c47f7b5327b
ChayaphonKultanon <chayaphon33793@gmail.com>
2025-09-07 12:45:51 +0700

    fix some typo

--- frontend/src/app/(user)/registration/password/page.tsx
+++ frontend/src/app/(user)/registration/password/page.tsx
@@ -1,9 +1,9 @@
-
+import RegistrationPassword from "@/components/registration/registration_password";
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center h-screen">
-        Password
+        <RegistrationPassword />
     </div>
   );
 }
