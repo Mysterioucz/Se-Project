41412c880f6a0ce075e56374fb2e9ed96ae130bf
Nantaporn <feihao28@gmail.com>
2025-09-08 15:05:09 +0700

    feat: add api register

---
+++ backend/controllers/auth.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from 'express';
+import prisma from '../db.js';
+import bcrypt from 'bcrypt';
+
+
+//@desc     Register account
+//@route    POST /api/v1/auth/register
+//@access   Public
+export const register = async (req: Request, res: Response) => {
+    const {AccountID, Password, FirstName, LastName} = req.body || {};
+    if (!AccountID || !Password || !FirstName || !LastName) {
+        return res
+          .status(500)
+          .json({error : "Please provide all require parameters"});
+    }
+    try {
+        const salt = 12;
+        const hashedPassword = await bcrypt.hash(Password, salt);
+
+        const newUser = await prisma.account.create({
+            data: {
+                AccountID,
+                Password : hashedPassword,
+                FirstName,
+                LastName
+            },
+        });
+        
+        res.status(200).json({
+            success : true,
+            data : newUser
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ 
+            message: 'Error' 
+        });
+    }
+};
+
+
--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import airportRouter from './routes/airports.js'
+import authRouter from './routes/auth.js'
+
 
 const app = express();
 app.use(express.json());
@@ -8,6 +10,7 @@ const PORT = process.env.PORT || 6000;
 
 // Mount API Router
 app.use('/api/v1/airports', airportRouter);
+app.use('/api/v1/auth', authRouter);
 
 // Start Server
 app.listen(PORT, () => {
