e17f855da091b2a2a265b06d42a65ccf09e27ede
ChayaphonKultanon <chayaphon33793@gmail.com>
2025-09-09 00:24:12 +0700

    update helper and create both modal

---
+++ frontend/src/components/modals/modal_delete_account.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Modal from "@components/Modal"; // adjust the path as needed
+
+interface ModalDeleteAccountProps {
+  isOpen: boolean;
+}
+
+export default function ModalDeleteAccount({
+  isOpen,
+}: ModalDeleteAccountProps) {
+  return (
+    <Modal
+      open={isOpen}
+      onClose={() => {}}
+      icon="/modal/fi-br-critical-warning.svg"
+      topic="Delete Account - CRITICAL WARNING"
+      subTopic="This action cannot be undone"
+      description="All your data, including bookings, preferences, and account information
+                  will be permanently deleted. You will lose access to all services and
+                  cannot recover your account."
+      leftButtonText="Cancel"
+      rightButtonText="Delete"
+      onLeftButton={() => {}}
+      onRightButton={() => {}}
+      variant="criticalwarning"
+      preventClose={false}
+      disableRightButton={false}
+    />
+  );
+}
