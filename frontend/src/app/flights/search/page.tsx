7f3d348371e94f35de4d50b44b30a6231f76af6e
Suvijak <suvijakworksemail@gmail.com>
2025-09-09 20:13:31 +0700

    fix/icon-in-footer

--- frontend/src/app/flights/search/page.tsx
+++ frontend/src/app/flights/search/page.tsx
@@ -1,5 +1,4 @@
 import Footer from "@components/footer/footer";
-import Sidebar, { SidebarItem } from "@components/sidebar/sidebar";
 import Image from "next/image";
 
 // test
@@ -7,53 +6,6 @@ export default function Page() {
     return (
         <div>
             <div>Flight Search</div>
-            {/* Example Sidebar Component */}
-            <Sidebar>
-                <SidebarItem
-                    label="Book Flight"
-                    href="/flights/search"
-                    logo={
-                        <Image
-                            src="/icons/icon_stroke/fi-br-plane.svg"
-                            alt="plane"
-                            className="w-5 h-5 aspect-square"
-                        />
-                    }
-                />
-                <SidebarItem
-                    label="My Bookings"
-                    href="/bookings"
-                    logo={
-                        <Image
-                            src="/icons/icon_stroke/fi-br-bookmark.svg"
-                            alt="bookmark"
-                            className="w-5 h-5 aspect-square"
-                        />
-                    }
-                />
-                <SidebarItem
-                    label="Profile"
-                    href="/profile"
-                    logo={
-                        <Image
-                            src="/icons/icon_stroke/fi-br-user.svg"
-                            alt="user"
-                            className="w-5 h-5 aspect-square"
-                        />
-                    }
-                />
-                <SidebarItem
-                    label="Settings"
-                    href="/settings"
-                    logo={
-                        <Image
-                            src="/icons/icon_stroke/fi-br-settings.svg"
-                            alt="settings"
-                            className="w-5 h-5 aspect-square"
-                        />
-                    }
-                />
-            </Sidebar>
             <Footer />
         </div>
     );
--- frontend/src/components/icons/module.ts
+++ frontend/src/components/icons/module.ts
@@ -1,1 +1,3 @@
 export { default as SmallArrowDown } from "./fi-sr-angle-small-down.svg";
+export { default as CallHistory } from "./fi-br-call-history.svg";
+export { default as EnvelopeIcon } from "./fi-br-envelope.svg";
