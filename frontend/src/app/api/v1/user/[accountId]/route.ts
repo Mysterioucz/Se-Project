3f41162cb5736f651847c245b4a66ffb056a8aeb
faith eiei <chyut2005@gmail.com>
2025-09-10 21:52:17 +0700

    fix: fix logic for updating account data

--- frontend/src/app/api/v1/user/[accountId]/route.ts
+++ frontend/src/app/api/v1/user/[accountId]/route.ts
@@ -1,6 +1,15 @@
 import prisma from "@/db";
 import { authorize } from "@/src/lib/authMiddleware";
 import { NextRequest } from "next/server";
+import jwt from "jsonwebtoken";
+import { decode } from "punycode";
+
+interface JwtPayload {
+    AccountID: string;
+    Email: string;
+    FirstName: string;
+    LastName: string;
+}
 
 export async function PUT(
     req: NextRequest,
@@ -20,6 +29,18 @@ export async function PUT(
     const { accountId } = await params;
     const { updateFirstName, updateLastName } = await req.json();
 
+    let token = req.headers.get("authorization")!.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
+    if (decoded.AccountID != accountId) {
+        return new Response(
+            JSON.stringify({
+                success: false,
+                message: 'Not authorized to this route'
+            }),
+            { status: 401 }
+        );
+    }
+
     if (! updateFirstName && ! updateLastName) {
         return new Response(
             JSON.stringify({
@@ -35,9 +56,9 @@ export async function PUT(
             where: { 
                 AccountID: accountId 
             },
-            data: { 
-                FirstName: updateFirstName, 
-                LastName: updateLastName 
+            data: {
+                ...(updateFirstName && { FirstName: updateFirstName }), // Only update if provided
+                ...(updateLastName && { LastName: updateLastName }),   // Only update if provided
             },
         });
 
