7f4127c865f8bafb39be80240072473a413f313b
Suvijak <suvijakworksemail@gmail.com>
2025-09-10 23:08:03 +0700

    chore/add-smth

--- frontend/src/app/flights/search/page.tsx
+++ frontend/src/app/flights/search/page.tsx
@@ -1,10 +1,12 @@
 import Footer from "@components/footer/footer"
+import Navbar from "@/src/components/Navbar";
 
 // test
 export default function Page() {
     return (
         <div>
             {/* Example Sidebar Component */}
+            <Navbar />
             <Footer />
         </div>
     );
