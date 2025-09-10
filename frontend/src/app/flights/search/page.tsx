7b0e2129c0cb032db0a6d5a7e26832fbe193eebb
Suvijak <suvijakworksemail@gmail.com>
2025-09-10 22:51:51 +0700

    fix/resolve-deployment-error

--- frontend/src/app/flights/search/page.tsx
+++ frontend/src/app/flights/search/page.tsx
@@ -1,9 +1,3 @@
-import {
-    FiBrBookmark,
-    FiBrPlane,
-    FiBrSettings,
-    FiBrUser,
-} from "@components/icons/module";
 import Footer from "@components/footer/footer"
 
 // test
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default async function Page() {
-<<<<<<< HEAD
-    // redirect("/flights/search");
-    redirect("/account-setting")
-=======
-	
     redirect("/flights/search");
->>>>>>> main
 }
