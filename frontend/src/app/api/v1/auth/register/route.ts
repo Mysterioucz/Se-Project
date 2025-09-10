c8290fb68d2fba5694f0e14da6f0f6fad8c38cc3
faith eiei <chyut2005@gmail.com>
2025-09-10 10:14:00 +0700

    fix: fix schema

--- frontend/src/app/api/v1/auth/register/route.ts
+++ frontend/src/app/api/v1/auth/register/route.ts
@@ -6,7 +6,7 @@ import { NextRequest } from "next/server";
 //@route    POST /api/v1/auth/register
 //@access   Public
 export async function POST(req: NextRequest) {
-    const { AccountID, Password, FirstName, LastName } = await req.json();
+    const { AccountID, Email, Password, FirstName, LastName } = await req.json();
     if (!AccountID || !Password || !FirstName || !LastName) {
         return new Response(
             JSON.stringify({ error: "Please provide all required parameters" }),
@@ -20,6 +20,7 @@ export async function POST(req: NextRequest) {
         const newUser = await prisma.account.create({
             data: {
                 AccountID,
+                Email,
                 Password: hashedPassword,
                 FirstName,
                 LastName,
