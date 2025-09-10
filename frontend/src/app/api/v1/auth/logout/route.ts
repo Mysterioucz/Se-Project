75fd271f44ab22f06a1e13c889337502f7c2f748
faith eiei <chyut2005@gmail.com>
2025-09-10 21:29:18 +0700

    feat: add authorization to logout function

--- frontend/src/app/api/v1/auth/logout/route.ts
+++ frontend/src/app/api/v1/auth/logout/route.ts
@@ -1,17 +1,23 @@
-//@desc   Log user out / clear token
-//@route  POST /api/v1/auth/logout
-
-// import { protect } from "@/src/lib/authMiddleware";
+import { authorize } from "@/src/lib/authMiddleware";
 import { cookies } from "next/headers";
 import { NextRequest } from "next/server";
 
-//@access Private
+//@desc     Log user out / clear token
+//@route    POST /api/v1/auth/logout
+//@access   Private
 export async function POST(req: NextRequest) {
-    // const protectedResponse = await protect(req);
-    // if (protectedResponse?.status == 401) {
-        // return protectedResponse;
-    // }
+    const protect = await authorize(req, ['Admin', 'User']);
+    if (protect.status == 200) {
+        return new Response(
+            JSON.stringify({
+                success: false,
+                message: 'Not authorized to this route'
+            }),
+            { status: 401 }
+        );
+    }
 
+    // disable cookie
     const cookieStore = await cookies();
     cookieStore.set("token", "", {
         expires: new Date(0),
