14d4b18eecc0b6136bc6387e918a0920c815c73b
Mysterioucz <chatrinza@gmail.com>
2025-09-12 18:45:59 +0700

    refactor: reorganize component structure and add session handling in flight search page

--- frontend/src/app/flights/search/page.tsx
+++ frontend/src/app/flights/search/page.tsx
@@ -8,10 +8,14 @@ import {
 import Footer from "@components/footer/footer";
 import Sidebar, { SidebarItem } from "@components/sidebar/sidebar";
 import FlightSearchBar from "@/src/components/flight_search/search";
-import SelectPassengerPanel, { PanelData } from "@/src/components/selectPassengerPanel/select_passenger_panel";
+import SelectPassengerPanel, {
+    PanelData,
+} from "@/src/components/selectPassengerPanel/select_passenger_panel";
 import FlightCard from "@/src/components/flightCard/flight_card";
 import { mockFlightData } from "@/src/data/mockFlightData";
 import { useState } from "react";
+import Navbar from "@/src/components/Navbar";
+import { getSession } from "next-auth/react";
 
 export default function Page() {
     const [panelData, setPanelData] = useState<PanelData>({
@@ -31,20 +35,26 @@ export default function Page() {
         }));
         console.log({ ...panelData, [key]: value });
     };
-
+    const session = getSession();
+	console.log(session);	
     return (
-        <div className="flex flex-col p-8">
-            <FlightCard
-                airlineTimeStamp={mockFlightData.airlineTimeStamp}
-                priceCabinClass={{
-                    ...mockFlightData.priceCabinClass,
-                    onClick: () => {},
-                }}
-            />
-            <SelectPassengerPanel
-                panelData={panelData}
-                onChange={handlePanelDataChange}
-            />
-        </div>
+        <>
+            <div className="flex flex-col">
+                <Navbar isSignIn={!!session} />
+                <div className="flex flex-col p-8">
+                    <FlightCard
+                        airlineTimeStamp={mockFlightData.airlineTimeStamp}
+                        priceCabinClass={{
+                            ...mockFlightData.priceCabinClass,
+                            onClick: () => {},
+                        }}
+                    />
+                    <SelectPassengerPanel
+                        panelData={panelData}
+                        onChange={handlePanelDataChange}
+                    />
+                </div>
+            </div>
+        </>
     );
 }
