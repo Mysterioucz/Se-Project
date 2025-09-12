0092d5c09cc2e419254ddfbdb3b73b035adb197d
Mysterioucz <chatrinyoonchalard@gmail.com>
2025-09-12 23:48:58 +0700

    refactor: remove unused imports and state management from flight search page

--- frontend/src/app/flights/search/page.tsx
+++ frontend/src/app/flights/search/page.tsx
@@ -1,40 +1,8 @@
 "use client";
-import {
-    FiBrBookmark,
-    FiBrPlane,
-    FiBrSettings,
-    FiBrUser,
-} from "@components/icons/module";
-import Footer from "@components/footer/footer";
-import Sidebar, { SidebarItem } from "@components/sidebar/sidebar";
-import FlightSearchBar from "@/src/components/flight_search/search";
-import SelectPassengerPanel, {
-    PanelData,
-} from "@/src/components/selectPassengerPanel/select_passenger_panel";
-import FlightCard from "@/src/components/flightCard/flight_card";
-import { mockFlightData } from "@/src/data/mockFlightData";
-import { useState } from "react";
-import { signOut, useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
 
 export default function Page() {
-    const [panelData, setPanelData] = useState<PanelData>({
-        adultCount: 1,
-        childCount: 0,
-        infantCount: 0,
-        seatClass: "",
-    });
 
-    const handlePanelDataChange = (
-        key: keyof PanelData,
-        value: number | string
-    ) => {
-        setPanelData((prev) => ({
-            ...prev,
-            [key]: value,
-        }));
-    };
-
-	const session = useSession();
     return (
         <div className="flex flex-col p-8">
 			{/* Logout Example */}
