83ca03646ea4e4ff9bfe239940e2800b0d83c7da
Suvijak <suvijakworksemail@gmail.com>
2025-09-12 23:21:37 +0700

    feat/add-funishing-component-in-flight-search-page

--- frontend/src/app/flights/search/page.tsx
+++ frontend/src/app/flights/search/page.tsx
@@ -1,12 +1,11 @@
 import Footer from "@components/footer/footer"
 import Navbar from "@/src/components/Navbar";
-
-// test
+import FlightSearchFunishing from "@components/flight_search/FlightSearchFurnishings";
 export default function Page() {
     return (
-        <div>
-            {/* Example Sidebar Component */}
-            <Navbar />
+        <div className="flex flex-col items-start gap-[var(--Spacing-xxl,40px)] h-dvh">
+            <Navbar displayName="John Doe" />
+            <FlightSearchFunishing />
             <Footer />
         </div>
     );
