7a1390a7cc62463f0f074b89aa059d0120ab6996
ChayaphonKultanon <chayaphon33793@gmail.com>
2025-09-15 18:43:03 +0700

    implement backend

--- frontend/src/components/modals/modal_delete_account.tsx
+++ frontend/src/components/modals/modal_delete_account.tsx
@@ -1,15 +1,40 @@
 "use client";
 
 import Modal from "@components/Modal"; // adjust the path as needed
+import { useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
 
 interface ModalDeleteAccountProps {
   isOpen: boolean;
+  onClose: () => void;
 }
 
-// TODO: connect with backend
 export default function ModalDeleteAccount({
   isOpen,
+  onClose,
 }: ModalDeleteAccountProps) {
+  const session = useSession();
+  const accountId = session?.data?.user?.id;
+  async function handleDeleteAccount() {
+    try {
+      const result = await fetch(`/api/v1/account/${accountId}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const resultData = await result.json();
+      if (resultData.success) {
+        signOut({ callbackUrl: "/login" });
+      } else {
+        console.error("Failed to delete account:", resultData.error);
+      }
+    } catch (error) {
+      console.error("Network error:", error);
+    }
+  }
+
   return (
     <Modal
       open={isOpen}
@@ -22,8 +47,11 @@ export default function ModalDeleteAccount({
                   cannot recover your account."
       leftButtonText="Cancel"
       rightButtonText="Yes, Delete Forever"
-      onLeftButton={() => {}}
-      onRightButton={() => {}}
+      onLeftButton={() => onClose()}
+      onRightButton={() => {
+        handleDeleteAccount();
+        onClose();
+      }}
       variant="criticalwarning"
       preventClose={false}
       disableRightButton={false}
