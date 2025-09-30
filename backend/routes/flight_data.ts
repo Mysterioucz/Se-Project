579e6d03cfae2432c3b3070c97c40700ac16a29f
Mysterioucz <chatrinyoonchalard@gmail.com>
2025-09-30 12:30:37 +0700

    refactor/merge-folder

--- backend/routes/flight_data.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getFlights, getFlightByNo } from '../controllers/flight_data.js';
-
-const router = express.Router();
-
-router.get('/', getFlights)
-router.get('/:flightNo', getFlightByNo);
-
-export default router;
--- backend/routes/profile.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getProfile } from '../controllers/profile.js';
-
-const router = Router();
-
-// GET /api/profile/:accountId
-router.get('/:accountId', getProfile);
-
-export default router;
