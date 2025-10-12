ff0d83028f7520c4011238c24ae7ed87f7e6e54e
Suvijak <suvijakworksemail@gmail.com>
2025-10-13 01:04:02 +0700

    feat/setup-layout-confirmation-page

--- src/app/(user)/checkout/payment/confirmation/page.tsx
+++ src/app/(user)/checkout/payment/confirmation/page.tsx
@@ -1,3 +1,30 @@
 export default function Page() {
-	return <div>Confirmation</div>;
+  return (
+
+    <div className="flex flex-col w-full justify-center gap-10">
+        <h1 className="font-sarabun text-[2rem] font-bold leading-[120%] text-primary-600">
+            Flight Order Summary
+        </h1>
+
+        <div className="flex items-start gap-16 self-stretch">
+            <div className="flex flex-col items-start gap-[0.625rem] w-[35rem] p-[1rem]">
+                <div className="flex items-start self-stretch px-4 py-2 rounded-md bg-primary-50">
+                    <div className="font-sarabun text-[1rem] font-semibold leading-[120%] text-primary-900 m-0">
+                        Type Trip : One-Way
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex items-start gap-[4rem] self-stretch">
+                {/* add more children here */}
+            </div>
+
+            <div className="flex flex-col items-start gap-[1rem] [flex:1_0_0] bg-blue-100 p-[1rem]">
+                <p>Some content</p>
+            </div>
+        </div>
+    </div>
+
+
+  );
 }
