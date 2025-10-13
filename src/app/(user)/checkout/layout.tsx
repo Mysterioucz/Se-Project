5e9bd823c822c4537debe489322c895351ee5f11
098ff <chanatdakc@gmail.com>
2025-10-13 17:19:44 +0700

    feat: Payment success page

--- src/app/(user)/checkout/layout.tsx
+++ src/app/(user)/checkout/layout.tsx
@@ -1,18 +1,23 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import CheckoutProgress from "@/src/components/checkout/CheckoutProgress";
 import PriceSummary from "@/src/components/checkout/PriceSummary";
 import Navbar from "@/src/components/Navbar";
 
 export default function CheckoutLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <div className="flex flex-col min-h-screen gap-8 items-center">
-            <Navbar />
-            <CheckoutProgress />
-            <div className="flex w-full px-32">{children}</div>
-            <PriceSummary />
-        </div>
-    );
+  const pathname = usePathname();
+  const hideProgress = pathname.includes("/checkout/payment/success");
+  return (
+    <div className="flex flex-col min-h-screen gap-8 items-center">
+      <Navbar />
+      {!hideProgress && <CheckoutProgress />}
+      <div className="flex w-full px-32">{children}</div>
+      {!hideProgress && <PriceSummary />}
+    </div>
+  );
 }
--- src/app/(user)/checkout/payment/page.tsx
+++ src/app/(user)/checkout/payment/page.tsx
@@ -93,7 +93,7 @@ export default function Page() {
         </div>
       </div>
 
-      {/* Right-side Bar */}
+      {/* TODO: Call Right side bar to be used here */}
       <div className="w-[21.25rem] h-[47.5625rem] bg-red-500"></div>
 
 	  <QRModal
