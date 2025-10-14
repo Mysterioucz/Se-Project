035c114cf056e6a44e9a8e50d0a3fc77a3feb1e0
faith eiei <chyut2005@gmail.com>
2025-10-15 01:42:02 +0700

    feat: implement API for fetching user's cart

--- src/lib/getCart.tsx
+++ src/lib/getCart.tsx
@@ -1,7 +1,7 @@
 export default async function getCart(UserAccountID:string) {
     const response = await fetch(`http://localhost:5000/api/v1/cart/${UserAccountID}`);
     if (! response.ok) {
-        throw new Error(`Failed to fetch cars`);
+        throw new Error(`Failed to fetch user's cart`);
     }
 
     return await response.json();
