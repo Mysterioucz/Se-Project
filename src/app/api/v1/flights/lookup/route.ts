fa705e47cba034d86627e3c4674366d218eebb5c
Mysterioucz <chatrinyoonchalard@gmail.com>
2025-10-21 02:19:38 +0700

    feat: enhance flight data fetching and seat selection components in checkout flow

--- src/app/api/v1/flights/lookup/route.ts
+++ src/app/api/v1/flights/lookup/route.ts
@@ -6,87 +6,96 @@ import { NextRequest } from "next/server";
 //@route    GET /api/v1/flights/lookup?flightNo=...&departTime=...&arrivalTime=...
 //@access   Public
 export async function GET(req: NextRequest) {
-  try {
-    const { searchParams } = req.nextUrl;
-    const flightNo = searchParams.get('flightNo');
-    const departTime = searchParams.get('departTime'); // format : 2025-09-27T01:25:20.000Z
-    const arrivalTime = searchParams.get('arrivalTime');
-     
-    if (!flightNo || !departTime || !arrivalTime) {
-      return new Response(
+    try {
+        const { searchParams } = req.nextUrl;
+        const flightNo = searchParams.get("flightNo");
+        const departTime = searchParams.get("departTime"); // format : 2025-09-27T01:25:20.000Z
+        const arrivalTime = searchParams.get("arrivalTime");
+
+        if (!flightNo || !departTime || !arrivalTime) {
+            return new Response(
+                JSON.stringify({
+                    success: false,
+                    error: ErrorMessages.MISSING_PARAMETER,
+                }),
+                { status: 400 },
+            );
+        }
+
+        const flight = await prisma.flight.findUnique({
+            where: {
+                FlightNo_DepartTime_ArrivalTime: {
+                    FlightNo: flightNo,
+                    DepartTime: departTime,
+                    ArrivalTime: arrivalTime,
+                },
+            },
+            select: {
+                availableServices: {
+                    select: {
+                        service: {
+                            select: {
+                                ServiceName: true,
+                                Price: true,
+                                Description: true,
+                            },
+                        },
+                    },
+                },
+                seats: {
+                    // It should retrieve both available and unavailable
+                    // where: {
+                    //   IsAvailable: true
+                    // },
+                    select: {
+                        SeatNo: true,
+                        SeatType: true,
+                        IsAvailable: true,
+                    },
+                },
+                arrivalAirport: true,
+                departureAirport: true,
+                ArrivalAirportID: true,
+                DepartureAirportID: true,
+            },
+        });
+
+        if (!flight) {
+            return new Response(
+                JSON.stringify({
+                    success: false,
+                    error: ErrorMessages.NOT_FOUND,
+                }),
+                { status: 404 },
+            );
+        }
+
+        const services = flight.availableServices.map(
+            (s) => s.service.ServiceName,
+        );
+
+        const responseData = {
+            services,
+            ...(services.includes("Select Seat") && {
+                availableSeats: flight.seats || [],
+            }),
+            flight
+        };
+
+        return new Response(
+            JSON.stringify({
+                success: true,
+                data: responseData,
+            }),
+            { status: 200 },
+        );
+    } catch (error) {
+        return new Response(
             JSON.stringify({
                 success: false,
-                error: ErrorMessages.MISSING_PARAMETER,
+                message: ErrorMessages.SERVER,
             }),
-            { status: 400 }
+            { status: 500 },
         );
     }
-
-    const flight = await prisma.flight.findUnique({
-      where: {
-        FlightNo_DepartTime_ArrivalTime: {
-          FlightNo: flightNo,
-          DepartTime: departTime,
-          ArrivalTime: arrivalTime,
-        },
-      },
-      select: {
-        availableServices: {
-          select: {
-            service: {
-              select: {
-                ServiceName: true,
-                Price: true,
-                Description: true,
-              }
-            }
-          }
-        },
-        seats: {
-          // It should retrieve both available and unavailable
-          // where: {
-          //   IsAvailable: true
-          // },
-          select: {
-            SeatNo: true,
-            SeatType: true,
-            IsAvailable: true
-          }
-        }
-      },
-    });
-
-    if (!flight) {
-      return new Response(
-        JSON.stringify({
-            success: false,
-            error: ErrorMessages.NOT_FOUND,
-        }),
-        { status: 404 }
-      );
-    }
-
-    const services = flight.availableServices.map(s => s.service.ServiceName);
-
-    const responseData = {
-      services,
-      ...(services.includes("Select Seat") && {availableSeats : flight.seats || []})
-    };
-
-    return new Response(
-      JSON.stringify({
-          success: true,
-          data: responseData,
-      }),
-      { status: 200 }
-    );
-  } catch (error) {
-    return new Response(
-      JSON.stringify({
-          success: false,
-          message: ErrorMessages.SERVER,
-      }),
-      { status: 500 }
-    );
-  }
 }
--- src/components/selectSeatCard/selectSeatCard.tsx
+++ src/components/selectSeatCard/selectSeatCard.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import Button from "@components/Button";
 import { FiSrPlane, FlightCardDivider } from "@components/icons/module";
@@ -16,15 +16,26 @@ interface SelectSeatCardProps {
     date: string;
     passengerCount: number;
     passengerType?: string;
-    class: string;
+    seatClass: string;
 }
 
-
-
-export default function SelectSeatCard({ header, departFrom, departFromFull, arriveAt, arriveAtFull, departTime, arriveTime, duration, date, passengerCount, passengerType, class: seatClass }: SelectSeatCardProps){
+export default function SelectSeatCard({
+    header,
+    departFrom,
+    departFromFull, // TODO: Backend not yet implement
+    arriveAt,
+    arriveAtFull, // TODO: Backend not yet implement
+    departTime,
+    arriveTime,
+    duration,
+    date,
+    passengerCount,
+    passengerType,
+    seatClass,
+}: SelectSeatCardProps) {
     const [selected, setSelected] = useState(false);
 
-    function PassengerName({ index }: { index: number }){
+    function PassengerName({ index }: { index: number }) {
         return (
             <div className="flex w-[15.3125rem] h-[3.1875rem] rounded-2xl bg-primary-50">
                 <div className="flex flex-row gap-2 justify-between items-center w-full px-4 py-[0.3125rem]">
@@ -32,73 +43,88 @@ export default function SelectSeatCard({ header, departFrom, departFromFull, arr
                         {/* TODO: implement passenger selection */}
                     </div>
                     <div className="w-[11.125rem] h-[2rem] rounded-lg p-[0.4375rem] bg-primary-100 items-center">
-                        <p className="!text-[0.875rem] !text-primary-400">Passenger Name</p>
+                        <p className="!text-[0.875rem] !text-primary-400">
+                            Passenger Name
+                        </p>
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 
-    function SeatMap(){
+    function SeatMap() {
         return (
             <div className="flex px-6 bg-white w-full h-[27.0625rem] items-center justify-center">
-                                <div className="flex flex-col gap-4 h-[24rem]">
-                                    <div className="flex flex-row gap-5">
-                                        {/* Seat Map */}
-                                        <div className="flex flex-row w-[29.1875rem] rounded-2xl p-8 g-8 bg-primary-50 items-center justify-center">
-                                            <div className="flex w-[2rem] h-[14.125rem] rounded-lg bg-white items-center justify-center">
-                                                <p className="!text-[0.875rem] !text-primary-600 -rotate-90">Font</p>
-                                            </div>
-                                            <div className="flex flex-col gap-2 overflow-x-auto py-2 px-2">
-                                                {[...Array(6)].map((_, rowIndex) => {
-                                                    const letter = String.fromCharCode(70 - rowIndex); // 70 = 'F', then E, D, C, B, A
-                                                    const row = (
-                                                        <div key={letter} className="flex gap-4 items-center">
-                                                            {[...Array(10)].map((_, colIndex) => (
-                                                                // Seat
-                                                                <div
-                                                                    key={colIndex}
-                                                                    className="min-w-8 min-h-8 p-[0.3125rem] bg-white rounded-lg flex items-center justify-center cursor-pointer hover:bg-primary-100"
-                                                                >
-                                                                    <p className="!text-[0.875rem] !text-primary-600 -rotate-90">
-                                                                        {colIndex + 1}{letter}
-                                                                    </p>
-                                                                </div>
-                                                            ))}
-                                                        </div>
-                                                    );
-
-                                                    // insert a small spacer (blank line) between D and C (after the first 3 rows: F,E,D)
-                                                    if (rowIndex === 3) {
-                                                        return [
-                                                            <div key="spacer" className="h-2" />,
-                                                            row
-                                                        ];
-                                                    }
-
-                                                    return row;
-                                                })}
-                                            </div>
-                                        </div>
-
-                                        {/* Passengers */}
-                                        <div className="flex flex-col gap-4">
-                                            {/* TODO: implement passenger selection to match seat selection */}
-                                            {[...Array(passengerCount)].map((_, index) => (
-                                                <PassengerName key={index} index={index} />
-                                            ))}
-                                        </div>
-                                    </div>
-                                    <Button
-                                        text="Confirm"
-                                        height="h-[2.0625rem]"
-                                        width="w-[45.875rem]"
-                                    />
-                                </div>
+                <div className="flex flex-col gap-4 h-[24rem]">
+                    <div className="flex flex-row gap-5">
+                        {/* Seat Map */}
+                        <div className="flex flex-row w-[29.1875rem] rounded-2xl p-8 g-8 bg-primary-50 items-center justify-center">
+                            <div className="flex w-[2rem] h-[14.125rem] rounded-lg bg-white items-center justify-center">
+                                <p className="!text-[0.875rem] !text-primary-600 -rotate-90">
+                                    Font
+                                </p>
                             </div>
-        )
+                            <div className="flex flex-col gap-2 overflow-x-auto py-2 px-2">
+                                {[...Array(6)].map((_, rowIndex) => {
+                                    const letter = String.fromCharCode(
+                                        70 - rowIndex,
+                                    ); // 70 = 'F', then E, D, C, B, A
+                                    const row = (
+                                        <div
+                                            key={letter}
+                                            className="flex gap-4 items-center"
+                                        >
+                                            {[...Array(10)].map(
+                                                (_, colIndex) => (
+                                                    // Seat
+                                                    <div
+                                                        key={colIndex}
+                                                        className="min-w-8 min-h-8 p-[0.3125rem] bg-white rounded-lg flex items-center justify-center cursor-pointer hover:bg-primary-100"
+                                                    >
+                                                        <p className="!text-[0.875rem] !text-primary-600 -rotate-90">
+                                                            {colIndex + 1}
+                                                            {letter}
+                                                        </p>
+                                                    </div>
+                                                ),
+                                            )}
+                                        </div>
+                                    );
+
+                                    // insert a small spacer (blank line) between D and C (after the first 3 rows: F,E,D)
+                                    if (rowIndex === 3) {
+                                        return [
+                                            <div
+                                                key="spacer"
+                                                className="h-2"
+                                            />,
+                                            row,
+                                        ];
+                                    }
+
+                                    return row;
+                                })}
+                            </div>
+                        </div>
+
+                        {/* Passengers */}
+                        <div className="flex flex-col gap-4">
+                            {/* TODO: implement passenger selection to match seat selection */}
+                            {[...Array(passengerCount)].map((_, index) => (
+                                <PassengerName key={index} index={index} />
+                            ))}
+                        </div>
+                    </div>
+                    <Button
+                        text="Confirm"
+                        height="h-[2.0625rem]"
+                        width="w-[45.875rem]"
+                    />
+                </div>
+            </div>
+        );
     }
-    
+
     return (
         <div className="flex p-6 rounded-lg bg-primary-50 h-fit w-fit">
             {/* Content */}
@@ -108,40 +134,64 @@ export default function SelectSeatCard({ header, departFrom, departFromFull, arr
                 <div className="flex flex-col w-[49.3125rem] h-fit rounded-sm border-2 border-primary-300">
                     {/* Top part */}
                     <div className="flex py-2 px-4 bg-primary-300">
-                        <p className="!font-semibold !text-[1.25rem] text-white">{header}: {departFrom} to {arriveAt}</p>
+                        <p className="!font-semibold !text-[1.25rem] text-white">
+                            {header}: {departFrom} to {arriveAt}
+                        </p>
                     </div>
                     <div className="flex flex-row h-fit w-full">
                         {/* Left part */}
                         <div className="flex flex-col py-3 gap-3 w-[36rem] bg-primary-50">
                             <div className="flex px-4 gap-3 items-center">
                                 <FiSrPlane />
-                                <p className="!font-semibold !text-[1.25rem] !text-primary-600">Airline</p>
-                                <p className="!text-[1rem] !text-primary-600">{date}</p>
+                                <p className="!font-semibold !text-[1.25rem] !text-primary-600">
+                                    Airline
+                                </p>
+                                <p className="!text-[1rem] !text-primary-600">
+                                    {date}
+                                </p>
                             </div>
                             <div className="flex flex-row px-8 pb-2 items-center">
                                 <div className="flex flex-col w-[6.25rem] px-4 pb-2">
-                                    <p className="!font-semibold !text-[1.5rem] !text-primary-600">{departTime}</p>
-                                    <p className="!text-[1rem] !text-primary-600">{departFrom}</p>
-                                    <p className="!text-[1rem] !text-primary-500">{departFromFull}</p>
+                                    <p className="!font-semibold !text-[1.5rem] !text-primary-600">
+                                        {departTime}
+                                    </p>
+                                    <p className="!text-[1rem] !text-primary-600">
+                                        {departFrom}
+                                    </p>
+                                    <p className="!text-[1rem] !text-primary-500">
+                                        {departFromFull}
+                                    </p>
                                 </div>
                                 <div className="flex flex-col w-fit py-1 px-2 gap-1 items-center">
-                                    <p className="!text-[1rem] !text-primary-500">{duration}</p>
+                                    <p className="!text-[1rem] !text-primary-500">
+                                        {duration}
+                                    </p>
                                     <FlightCardDivider />
                                 </div>
                                 <div className="flex flex-col w-fit px-4 pb-2">
-                                    <p className="!font-semibold !text-[1.5rem] !text-primary-600">{arriveTime}</p>
-                                    <p className="!text-[1rem] !text-primary-600">{arriveAt}</p>
-                                    <p className="!text-[1rem] !text-primary-500">{arriveAtFull}</p>
+                                    <p className="!font-semibold !text-[1.5rem] !text-primary-600">
+                                        {arriveTime}
+                                    </p>
+                                    <p className="!text-[1rem] !text-primary-600">
+                                        {arriveAt}
+                                    </p>
+                                    <p className="!text-[1rem] !text-primary-500">
+                                        {arriveAtFull}
+                                    </p>
                                 </div>
                             </div>
                         </div>
                         {/* Right part */}
                         <div className="flex flex-col w-[13.3125rem] h-auto px-4 gap-3 bg-primary-100 justify-center items-center">
                             <div className="flex flex-col gap-1 items-end w-full">
-                                <p className="!text-[1rem] !text-primary-600">{passengerCount} {passengerType}</p>
-                                <p className="!font-semibold !text-[1.125rem] !text-primary-600">{seatClass}</p>
+                                <p className="!text-[1rem] !text-primary-600">
+                                    {passengerCount} {passengerType}
+                                </p>
+                                <p className="!font-semibold !text-[1.125rem] !text-primary-600">
+                                    {seatClass}
+                                </p>
                             </div>
-                            <Button 
+                            <Button
                                 text="Select Seat"
                                 height="h-[2.1875rem]"
                                 width="w-[11.3125rem]"
@@ -150,11 +200,9 @@ export default function SelectSeatCard({ header, departFrom, departFromFull, arr
                         </div>
                     </div>
                     {/* Bottom part */}
-                    { selected &&
-                        <SeatMap />
-                    }
+                    {selected && <SeatMap />}
                 </div>
             </div>
         </div>
-    )
+    );
 }
