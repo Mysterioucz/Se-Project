c9fb9190f684a416c386457cfe38722daa7d6f7b
Mysterioucz <chatrinyoonchalard@gmail.com>
2025-10-27 12:11:36 +0700

    fix: update type definition for Swagger component spec to use unknown instead of any

--- src/app/api-doc/_components/react-swagger.tsx
+++ src/app/api-doc/_components/react-swagger.tsx
@@ -3,7 +3,7 @@ import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
 interface Props {
-    spec: Record<string, any>;
+    spec: Record<string, unknown>;
 }
 
 export default function ReactSwagger({ spec }: Props) {
