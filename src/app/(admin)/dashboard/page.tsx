265c55e6a9aee9ab4ef56803c1aac1ef6fc95085
Suvijak <suvijakworksemail@gmail.com>
2025-11-01 19:36:21 +0700

    feat/add-admin-dashboard-page

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,3 +1,18 @@
+"use client";
+import { useSession } from "next-auth/react";
+import AdminNavBar from "./_components/AdminNavBar";
+
 export default function Page() {
-  return <div>Admin dashboard</div>;
+    const { data: session, status } = useSession();
+    const displayName = session?.user?.name;
+    return (
+        <div className="flex flex-col w-full justify-center gap-10 py-md">
+            <AdminNavBar />
+            <div className="flex flex-col items-start self-stretch px-[var(--Spacing-xxxxl,128px)] gap-[var(--Spacing-sm-md,12px)]">
+                <div className="text-primary-900 font-sarabun text-[48px] font-bold leading-[120%] not-italic">
+                    Welcome Back, {displayName}
+                </div>
+            </div>
+        </div>
+    );
 }
