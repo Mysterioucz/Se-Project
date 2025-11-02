540f863ec944b3cb7ccc0b6ecab32bdada515074
Suvijak <suvijakworksemail@gmail.com>
2025-11-02 22:51:05 +0700

    feat/add-report-management-header

--- src/app/(admin)/dashboard/_components/ReportManagement.tsx
+++ src/app/(admin)/dashboard/_components/ReportManagement.tsx
@@ -46,9 +46,9 @@ export default function ReportManagement() {
                             width="w-[12.5rem]"
                             height="h-[2rem]" 
                         >
+                            <MenuItem value="All Priority">All Priority</MenuItem>
                             <MenuItem value="Normal">Normal</MenuItem>
                             <MenuItem value="High">High</MenuItem>
-                            <MenuItem value="All Priority">All Priority</MenuItem>
                         </SelectComponent>
                     </div>
                     <div className="flex flex-col items-start gap-1">
@@ -71,6 +71,55 @@ export default function ReportManagement() {
                             <MenuItem value="Resolved">Resolved</MenuItem>
                         </SelectComponent>
                     </div>
+                </div>
+            </div>
+
+            <div className="flex flex-col items-center self-stretch px-4 pb-4 pt-0">
+                <div className="flex flex-col items-start gap-2 pb-2 rounded-md border-2 border-[var(--color-primary-100)]">
+                    {/* header */}
+                    <div className="flex items-start gap-2 self-stretch bg-[var(--color-primary-100)]">
+                        <div className="flex flex-col justify-center items-center w-[3.75rem] h-[3.125rem] py-[0.5rem]">
+                            <div className="text-black text-center font-sarabun text-[1.25rem] font-semibold leading-[1.2]">
+                                No.
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col justify-center items-center w-[9.875rem] h-[3.125rem] py-[0.5rem]">
+                            <div className="text-black text-center font-sarabun text-[1.25rem] font-semibold leading-[1.2]">
+                                Priority Level
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col justify-center items-center w-[9.875rem] h-[3.125rem] py-[0.5rem]">
+                            <div className="text-black text-center font-sarabun text-[1.25rem] font-semibold leading-[1.2]">
+                                Status
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col justify-center items-center w-[11.25rem] h-[3.125rem] py-[0.5rem]">
+                            <div className="text-black text-center font-sarabun text-[1.25rem] font-semibold leading-[1.2]">
+                                Problem Type
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col justify-center items-center w-[11.25rem] h-[3.125rem] py-[0.5rem]">
+                            <div className="text-black text-center font-sarabun text-[1.25rem] font-semibold leading-[1.2]">
+                                Submitted
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col justify-center items-center w-[11.25rem] h-[3.125rem] py-[0.5rem]">
+                            <div className="text-black text-center font-sarabun text-[1.25rem] font-semibold leading-[1.2]">
+                                Last Update
+                            </div>
+                        </div>
+
+                        {/* Space */}
+                        <div className="flex flex-col justify-center items-center w-[5.625rem] h-[3.125rem] py-[0.5rem]"></div>
+                    </div>
+
+
+                    {/* children */}
                 </div>
             </div>
         </div>
